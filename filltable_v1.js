1365ad0ffc583fc8e537279f4d109724ec9036d9
bjille <tim.vanloocke@gmail.com>
2017-11-03 15:27:08 +0100

    added no data selected popup

--- filltable_v1.js
+++ filltable_v1.js
@@ -4,8 +4,15 @@ var data;
 
 var selectionarray;
 
+function Resetselectionarray() {
+	selectionarray = [];
+	selectionarray.push(["period"]);
+	selectionarray.push(["geoitem"]);
+}
+
 $(document).ready(function(){
 
+	Resetselectionarray();
     
     $("#datatable").addClass('table table-striped');
 
@@ -17,14 +24,17 @@ $(document).ready(function(){
 		    });
 		});
 
+
+
 	$("#sel").change(function() {
-		var x = $("#sel").val();
+		var selection = $("#sel").val();
 		$("#result").empty();
-		selectionarray = [];
+		Resetselectionarray();
 		//var x = $("#sel option:selected").text();
-		selectionarray.push(["period"]);
-		selectionarray.push(["geoitem"]);
-		$.each(x, function(i,field){
+		//selectionarray = [];
+		//selectionarray.push(["period"]);
+		//selectionarray.push(["geoitem"]);
+		$.each(selection, function(i,field){
 			//alert(field)
 			$.each(dropdowndata, function(i,item){
 				if (item.Naam == field) {
@@ -49,27 +59,33 @@ $(document).ready(function(){
 	    data = json;
 	    var tbl_body = "";
 	    //$("#datatbody").append("<th>" + "period" + "</th>");
+	    if (selectionarray.length == 2){
+	    	alert("geen parameters geselecteerd");
+	    } else  {
 	    $.each(selectionarray, function(i,item){
 			$("#datatbody").append("<th>" + item + "</th>");
+			});
+		    	    
+		    //$("#datatbody").append("</tr>");
+		    //$("#datatbody").append("<tr><th>" + "period" + "</th><th>" + selectionarray[0] + "</th></th>");
+		    
+		    $.each(data, function(i, item) {
+		    	var tbl_row = "";
 
-	    });
-	    //$("#datatbody").append("</tr>");
-	    //$("#datatbody").append("<tr><th>" + "period" + "</th><th>" + selectionarray[0] + "</th></th>");
-	    
-	    $.each(data, function(i, item) {
-	    	var tbl_row = "";
+		    	$.each(this, function(k , v) {
+		    		$.each(selectionarray, function(k2,v2){
+		    			if (k == v2) {
+		    				tbl_row += "<td>"+v+"</td>";
+		    			}
+					
+					});
+		    	})
 
-	    	$.each(this, function(k , v) {
-	    		$.each(selectionarray, function(k2,v2){
-	    			if (k == v2) {
-	    				tbl_row += "<td>"+v+"</td>";
-	    			}
-				
-				});
-	    	})
+			    tbl_body += "<tr>"+tbl_row+"</tr>";
+			});	
+		}	
 
-		    tbl_body += "<tr>"+tbl_row+"</tr>";
-		});	
+
 
 		$("#datatable").addClass('table table-striped');
 		//$("#datatable").html("<tbody>" + tbl_body + "</tbody>");
