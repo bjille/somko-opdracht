f4f008c374e21a6eb6b41fd35497c60981dbed69
bjille <tim.vanloocke@gmail.com>
2017-11-08 17:18:23 +0100

    geoitem selectie toegevoegd

--- filltable_v1.js
+++ filltable_v1.js
@@ -1,98 +1,138 @@
 var dropdowndata;
 
-var data;
+var data = "";
 
-var selectionarray;
+var catarraysel;
 
-function Resetselectionarray() {
-	selectionarray = [];
-	selectionarray.push(["period"]);
-	selectionarray.push(["geoitem"]);
+var sectorarray = [];
+
+var sectorarraysel;
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
 }
+
+function FillGeoarray() {
+
+}
+
+
+function Resetcatarraysel() {
+	catarraysel = [];
+	catarraysel.push(["period"]);
+	catarraysel.push(["geoitem"]);
+}
+
+$.getJSON("bevolkingssamenstellingoverzicht.json", function(json){
+	data = json;
+});
+
+$.getJSON("overzichtdemografischeonderwerpen.json", function(json){
+	dropdowndata = json;
+});
+
 
 $(document).ready(function(){
 
-	Resetselectionarray();
+	Resetcatarraysel();
     
     $("#datatable").addClass('table table-striped');
 
+    $.each(dropdowndata, function(i,item) {
+    	$('#sel1').append("<option>" + item.Naam + "</option>");
+    });
 
-		$.getJSON("overzichtdemografischeonderwerpen.json", function(json){
-	    dropdowndata = json;
-		    $.each(dropdowndata, function(i,item) {
-		    	$('#sel').append("<option>" + item.Naam + "</option>");
-		    });
-		});
+    $.each(data, function(i, item) {
+		//sectorarray.push(item.geoitem);
+		//console.log($.inarray(item.geoitem, sectorarray));
+		if(($.inArray(item.geoitem,sectorarray)) == -1) {
+			$('#sel2').append("<option>" + item.geoitem + "</option>");
+			sectorarray.push(item.geoitem);
+	}
+	
+	});
 
-
-
-	$("#sel").change(function() {
-		var selection = $("#sel").val();
+	$("#sel1").change(function() {
+		var selection = $("#sel1").val();
 		$("#result").empty();
-		Resetselectionarray();
-		//var x = $("#sel option:selected").text();
-		//selectionarray = [];
-		//selectionarray.push(["period"]);
-		//selectionarray.push(["geoitem"]);
+		Resetcatarraysel();
+		//var x = $("#sel1 option:selected").text();
+		//catarraysel = [];
+		//catarraysel.push(["period"]);
+		//catarraysel.push(["geoitem"]);
 		$.each(selection, function(i,field){
 			//alert(field)
 			$.each(dropdowndata, function(i,item){
 				if (item.Naam == field) {
 					code = this.Onderwerpcode;
-					selectionarray.push([code]);
-					//selectionarray.push(i++);
-					//selectionarray = $.makeArray(dropdowndata);
-					$("#result").append("<p>" + item.Onderwerpcode + " = " + item.Beschrijving + "</p>");
+					catarraysel.push([code]);
+					//catarraysel.push(i++);
+					//catarraysel = $.makeArray(dropdowndata);
+					$("#result").append("<p>" + item.Beschrijving + " <b>(" + item.Onderwerpcode + ")</b>" +"</p>");
 					
 				}
 			});
 		});
 	});
 	
+	$("#sel2").change(function() {
+		var selection = $("#sel2").val();
+		sectorarraysel = [];
+		$.each(selection, function(i,item){
+			sectorarraysel.push(item);
+		});
+	});
+
 	$("button").click(function(){	
 
 	    var result;
-		var data;
+		//var data;
+		$("#datathead").empty();
 		$("#datatbody").empty();
+		var tbl_body = "";
 
-		$.getJSON("bevolkingssamenstellingoverzicht.json", function(json){
+		/*$.getJSON("bevolkingssamenstellingoverzicht.json", function(json){
 	    data = json;
-	    var tbl_body = "";
+	    */
 	    //$("#datatbody").append("<th>" + "period" + "</th>");
-	    if (selectionarray.length == 2){
+	    if (catarraysel.length == 2 && sectorarraysel.length == 0){
 	    	alert("geen parameters geselecteerd");
 
-	    } else  {
-	    $.each(selectionarray, function(i,item){
-			$("#datatbody").append("<th>" + item + "</th>");
-			});
+		    } else  {
+		    $.each(catarraysel, function(i,item){
+				$("#datathead").append("<th>" + item + "</th>");
+				});
 
-		    //$("#datatbody").append("</tr>");
-		    //$("#datatbody").append("<tr><th>" + "period" + "</th><th>" + selectionarray[0] + "</th></th>");
-		    
-		    $.each(data, function(i, item) {
-		    	var tbl_row = "";
+			    //$("#datathead").append("</tr>");
+			    //$("#datathead").append("<tr><th>" + "period" + "</th><th>" + catarraysel[0] + "</th></th>");
+			    
+			    $.each(data, function(i, item) {
+			    	var tbl_row = "";
+			    	//if (item.period == 2000 && item.geoitem == "A00") {
+			    	//if (item.geoitem == "A00") {
+			    	if(($.inArray(item.geoitem,sectorarraysel)) != -1) {
+			    		$.each(this, function(k , v) {
+			    		
+				    		$.each(catarraysel, function(k2,v2){
+				    			if (k == v2) {
+				    				tbl_row += "<td>"+v+"</td>";
+				    			}
+							});
+				    	})
+			    	}
 
-		    	$.each(this, function(k , v) {
-		    		$.each(selectionarray, function(k2,v2){
-		    			if (k == v2) {
-		    				tbl_row += "<td>"+v+"</td>";
-		    			}
-					
-					});
-		    	})
-
-			    tbl_body += "<tr>"+tbl_row+"</tr>";
-			});	
-		}	
-
-
+				    tbl_body += "<tr>"+tbl_row+"</tr>";
+				
+				});	
+			}	
 
 		$("#datatable").addClass('table table-striped');
+		$("#datathead").addClass('thead-inverse');
 		//$("#datatable").html("<tbody>" + tbl_body + "</tbody>");
 		//$("#datatbody").html(header + tbl_body);
 		$("#datatbody").append(tbl_body);
-		});
+
+		//});
 	});
 
 
